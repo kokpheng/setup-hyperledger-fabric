9f9de98f26d8dd30a60b6e7b824c1502e9e79f26
Kokpheng <yinkokpheng@gmail.com>
2020-01-08 18:07:41 +0900

    no message

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,42 +26,28 @@ printHelp() {
     echo "-s : bypass fabric-samples repo clone"
     echo "-b : bypass download of platform-specific binaries"
     echo
-    echo "e.g. bootstrap.sh 1.4.3 -s"
-    echo "would download docker images and binaries for version 1.4.3"
+    echo "e.g. bootstrap.sh 1.4.4 -s"
+    echo "would download docker images and binaries for version 1.4.4"
 }
 
-# dockerFabricPull() pulls docker images from fabric and chaincode repositories
+# dockerPull() pulls docker images from fabric and chaincode repositories
 # note, if a docker image doesn't exist for a requested release, it will simply
 # be skipped, since this script doesn't terminate upon errors.
-dockerFabricPull() {
-    local FABRIC_TAG=$1
-    for IMAGES in peer orderer ccenv tools baseos nodeenv javaenv; do
-        echo "==> FABRIC IMAGE: $IMAGES"
-        echo
-        docker pull "hyperledger/fabric-$IMAGES:$FABRIC_TAG"
-        docker tag "hyperledger/fabric-$IMAGES:$FABRIC_TAG" "hyperledger/fabric-$IMAGES"
+
+dockerPull() {
+    image_tag=$1
+    shift
+    while [[ $# -gt 0 ]]
+    do
+        image_name="$1"
+        echo "====> hyperledger/fabric-$image_name:$image_tag"
+        docker pull "hyperledger/fabric-$image_name:$image_tag"
+        docker tag "hyperledger/fabric-$image_name:$image_tag" "hyperledger/fabric-$image_name"
+        shift
     done
 }
 
-dockerThirdPartyImagesPull() {
-    local THIRDPARTY_TAG=$1
-    for IMAGES in couchdb kafka zookeeper; do
-        echo "==> THIRDPARTY DOCKER IMAGE: $IMAGES"
-        echo
-        docker pull "hyperledger/fabric-$IMAGES:$THIRDPARTY_TAG"
-        docker tag "hyperledger/fabric-$IMAGES:$THIRDPARTY_TAG" "hyperledger/fabric-$IMAGES"
-    done
-}
-
-dockerCaPull() {
-    local CA_TAG=$1
-    echo "==> FABRIC CA IMAGE"
-    echo
-    docker pull "hyperledger/fabric-ca:$CA_TAG"
-    docker tag "hyperledger/fabric-ca:$CA_TAG" "hyperledger/fabric-ca"
-}
-
-samplesInstall() {
+cloneSamplesRepo() {
     # clone (if needed) hyperledger/fabric-samples and checkout corresponding
     # version to the binaries and docker images to be downloaded
     if [ -d first-network ]; then
@@ -79,93 +65,66 @@ samplesInstall() {
     fi
 }
 
-# Incrementally downloads the .tar.gz file locally first, only decompressing it
-# after the download is complete. This is slower than binaryDownload() but
-# allows the download to be resumed.
-binaryIncrementalDownload() {
-    local BINARY_FILE=$1
-    local URL=$2
-    curl -f -s -C - "${URL}" -o "${BINARY_FILE}" || rc=$?
-    # Due to limitations in the current Nexus repo:
-    # curl returns 33 when there's a resume attempt with no more bytes to download
-    # curl returns 2 after finishing a resumed download
-    # with -f curl returns 22 on a 404
-    if [ "$rc" = 22 ]; then
-        # looks like the requested file doesn't actually exist so stop here
-        return 22
-    fi
-    if [ -z "$rc" ] || [ $rc -eq 33 ] || [ $rc -eq 2 ]; then
-        # The checksum validates that RC 33 or 2 are not real failures
-        echo "==> File downloaded. Verifying the md5sum..."
-        localMd5sum=$(md5sum "${BINARY_FILE}" | awk '{print $1}')
-        remoteMd5sum=$(curl -s "${URL}".md5)
-        if [ "$localMd5sum" == "$remoteMd5sum" ]; then
-            echo "==> Extracting ${BINARY_FILE}..."
-            tar xzf ./"${BINARY_FILE}" --overwrite
-            echo "==> Done."
-            rm -f "${BINARY_FILE}" "${BINARY_FILE}".md5
-        else
-            echo "Download failed: the local md5sum is different from the remote md5sum. Please try again."
-            rm -f "${BINARY_FILE}" "${BINARY_FILE}".md5
-            exit 1
-        fi
-    else
-        echo "Failure downloading binaries (curl RC=$rc). Please try again and the download will resume from where it stopped."
-        exit 1
-    fi
-}
-
-# This will attempt to download the .tar.gz all at once, but will trigger the
-# binaryIncrementalDownload() function upon a failure, allowing for resume
-# if there are network failures.
-binaryDownload() {
+# This will download the .tar.gz
+download() {
     local BINARY_FILE=$1
     local URL=$2
     echo "===> Downloading: " "${URL}"
-    # Check if a previous failure occurred and the file was partially downloaded
-    if [ -e "${BINARY_FILE}" ]; then
-        echo "==> Partial binary file found. Resuming download..."
-        binaryIncrementalDownload "${BINARY_FILE}" "${URL}"
+    wget "${URL}" || rc=$?
+    tar xvzf "${BINARY_FILE}" || rc=$?
+    rm "${BINARY_FILE}"
+    if [ -n "$rc" ]; then
+        echo "==> There was an error downloading the binary file."
+        return 22
     else
-        wget -O "${BINARY_FILE}" "${URL}" | tar xz || rc=$?
-        if [ -n "$rc" ]; then
-            echo "==> There was an error downloading the binary file. Switching to incremental download."
-            echo "==> Downloading file..."
-            binaryIncrementalDownload "${BINARY_FILE}" "${URL}"
-        else
-            echo "==> Done."
-        fi
+        echo "==> Done."
     fi
 }
 
-binariesInstall() {
+pullBinaries() {
     echo "===> Downloading version ${FABRIC_TAG} platform specific fabric binaries"
-    binaryDownload "${BINARY_FILE}" "https://github.com/hyperledger/fabric/releases/download/v${VERSION}/${BINARY_FILE}"
+    download "${BINARY_FILE}" "https://github.com/hyperledger/fabric/releases/download/v${VERSION}/${BINARY_FILE}"
     if [ $? -eq 22 ]; then
         echo
         echo "------> ${FABRIC_TAG} platform specific fabric binary is not available to download <----"
         echo
+        exit
     fi
 
     echo "===> Downloading version ${CA_TAG} platform specific fabric-ca-client binary"
-    binaryDownload "${CA_BINARY_FILE}" "hhttps://github.com/hyperledger/fabric-ca/releases/download/v${FABRIC_CA_TAG}/${CA_BINARY_FILE}"
+    download "${CA_BINARY_FILE}" "https://github.com/hyperledger/fabric-ca/releases/download/v${CA_VERSION}/${CA_BINARY_FILE}"
     if [ $? -eq 22 ]; then
         echo
         echo "------> ${CA_TAG} fabric-ca-client binary is not available to download  (Available from 1.1.0-rc1) <----"
         echo
+        exit
     fi
 }
 
-dockerInstall() {
+pullDockerImages() {
     command -v docker >& /dev/null
     NODOCKER=$?
     if [ "${NODOCKER}" == 0 ]; then
+        FABRIC_IMAGES=(peer orderer ccenv tools)
+        case "$VERSION" in
+        1.*)
+            FABRIC_IMAGES+=(javaenv)
+            shift
+            ;;
+        2.*)
+            FABRIC_IMAGES+=(nodeenv baseos javaenv)
+            shift
+            ;;
+        esac
+        echo "FABRIC_IMAGES:" "${FABRIC_IMAGES[@]}"
         echo "===> Pulling fabric Images"
-        dockerFabricPull "${FABRIC_TAG}"
+        dockerPull "${FABRIC_TAG}" "${FABRIC_IMAGES[@]}"
         echo "===> Pulling fabric ca Image"
-        dockerCaPull "${CA_TAG}"
+        CA_IMAGE=(ca)
+        dockerPull "${CA_TAG}" "${CA_IMAGE[@]}"
         echo "===> Pulling thirdparty docker images"
-        dockerThirdPartyImagesPull "${THIRDPARTY_TAG}"
+        THIRDPARTY_IMAGES=(zookeeper kafka couchdb)
+        dockerPull "${THIRDPARTY_TAG}" "${THIRDPARTY_IMAGES[@]}"
         echo
         echo "===> List out hyperledger docker images"
         docker images | grep hyperledger
@@ -225,19 +184,19 @@ done
 
 if [ "$SAMPLES" == "true" ]; then
     echo
-    echo "Installing hyperledger/fabric-samples repo"
+    echo "Clone hyperledger/fabric-samples repo"
     echo
-    samplesInstall
+    cloneSamplesRepo
 fi
 if [ "$BINARIES" == "true" ]; then
     echo
-    echo "Installing Hyperledger Fabric binaries"
+    echo "Pull Hyperledger Fabric binaries"
     echo
-    binariesInstall
+    pullBinaries
 fi
 if [ "$DOCKER" == "true" ]; then
     echo
-    echo "Installing Hyperledger Fabric docker images"
+    echo "Pull Hyperledger Fabric docker images"
     echo
-    dockerInstall
+    pullDockerImages
 fi
